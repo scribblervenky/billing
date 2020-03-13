03b6d1e3dd946ec85a7d902459c7cc1c1c487f17
scribblervenky <catchupvenky@gmail.com>
2020-03-13 17:15:07 +0530

    adding invoacation logic

--- src/containers/AddTodo.js
+++ src/containers/AddTodo.js
@@ -1,20 +1,33 @@
 import React from 'react'
 import { connect } from 'react-redux'
 import { addTodo } from '../actions'
+import { API, graphqlOperation } from "aws-amplify";
+import * as mutations from '../graphql/mutations';
 
 const AddTodo = ({ dispatch }) => {
   let input
 
+  const submit = async e => {
+    e.preventDefault()
+    if (!input.value.trim()) {
+      return
+    }
+    // Mutation
+    const todoDetails = {
+      id: Math.trunc(Math.random()*100), 
+      name: 'input.value',
+      description: 'TODO all over'
+    };
+
+    const newTodo = await API.graphql(graphqlOperation(mutations.createTodo, {input: todoDetails}));
+    console.log(newTodo);
+    dispatch(addTodo(input.value))
+    input.value = ''
+  }
+
   return (
     <div>
-      <form onSubmit={e => {
-        e.preventDefault()
-        if (!input.value.trim()) {
-          return
-        }
-        dispatch(addTodo(input.value))
-        input.value = ''
-      }}>
+      <form onSubmit={submit}>
         <input ref={node => input = node} />
         <button type="submit">
           Add Todo
